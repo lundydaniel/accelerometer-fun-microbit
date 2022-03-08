0d8dcbbf34dae4335e66ff14df4454afc0623c47
Dan Lundy <72224080+lundydaniel@users.noreply.github.com>
2022-03-07 20:54:10 -0500

    Fun little accelerometer project that plays sound.

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let alarm_triggered = false
+let z = input.acceleration(Dimension.Z)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+        z = input.acceleration(Dimension.Z)
+        alarm_triggered = false
+        basic.clearScreen()
+    }
+    if (Math.abs(input.acceleration(Dimension.Z) - z) > 160) {
+        alarm_triggered = true
+        if (alarm_triggered) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        }
+    }
 })
